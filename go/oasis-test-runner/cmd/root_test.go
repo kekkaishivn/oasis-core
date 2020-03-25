a6533712ee0da527852212fb7249b308d7bfab38
Matevž Jekovec <matevz.jekovec@oasislabs.com>
2020-03-25 13:20:23 +0100

    oasis-test-runner: Fix unit test

--- go/oasis-test-runner/cmd/root_test.go
+++ go/oasis-test-runner/cmd/root_test.go
@@ -14,10 +14,10 @@ func TestComputeParamSets(t *testing.T) {
 
 	expectedParamSets := []map[string]string{
 		{"testParam1": "1", "testParam2": "a"},
-		{"testParam1": "1", "testParam2": "b"},
 		{"testParam1": "2", "testParam2": "a"},
-		{"testParam1": "2", "testParam2": "b"},
 		{"testParam1": "3", "testParam2": "a"},
+		{"testParam1": "1", "testParam2": "b"},
+		{"testParam1": "2", "testParam2": "b"},
 		{"testParam1": "3", "testParam2": "b"},
 	}
 
